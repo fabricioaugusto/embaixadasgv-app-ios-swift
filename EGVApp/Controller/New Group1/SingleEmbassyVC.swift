e8aa58e2ae27e266f4e6c46b718ad7bfb9091919
Fabrício Augusto <fabricio.mata@gmail.com>
2019-11-13 11:55:05 -0800

    Criado tela Single User e Single Event

---
+++ EGVApp/Controller/New Group1/SingleEmbassyVC.swift
@@ -0,0 +1,30 @@
+//
+//  SingleEmbassyVC.swift
+//  EGVApp
+//
+//  Created by Fabricio on 13/11/19.
+//  Copyright © 2019 Fabrício Augusto. All rights reserved.
+//
+
+import UIKit
+
+class SingleEmbassyVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
